801261b1111b919973b43fc7c123baa283397530
leander006 <leanderdsilva06@gmail.com>
2024-03-07 23:24:54 +0530

    added spinner and smooth scroller

--- app/[coin]/page.tsx
+++ app/[coin]/page.tsx
@@ -13,6 +13,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { toast } from 'react-hot-toast';
 import {Spinner} from "@nextui-org/react";
 import type { RootState } from '@/redux/store'
+import ClipLoader from "react-spinners/ClipLoader";
 import { fetchDataMainFailure, fetchDataMainStart, fetchDataMainSuccess } from "@/redux/slice/mainPageSlice";
 import axios from "axios";
 export default function Home() {
@@ -51,53 +52,61 @@ export default function Home() {
 
   return (
     <>    
-      <div className="pt-24 px-6 lg:px-12 capitalize mb-3 text-[#3E5765] flex">
-            <h1>Cryptocurrencies {`>>`} </h1>
-            <h1 className="ml-2 text-black">{`${id}`}</h1>
-        </div>
-      <div className="flex flex-col lg:flex-row w-screen px-6 lg:px-12">
+{main?<div>
 
-        {loading ?<div className="lg:w-[70%] flex flex-col">
-              <div className="flex space-x-8 p-6 whitespace-nowrap mr-4 lg:bg-white lg:rounded-[8px] lg:rounded-b-none rounded-none">
-                      <div className="flex space-x-2 items-center">
-                      {/* // eslint-disable-next-line */}
-                        <img src={main?.image?.thumb} alt="mainLogo" />
-                            <h1 className="text-lg capitalize">{main?.name}</h1>
-                            <h3 className="text-sm capitalize">{main?.symbol}</h3>
-                      </div>
-                      <div className="bg-gray-500 p-3 text-white w-fit rounded-[8px] ">
-                            Rank #{main?.market_cap_rank}
-                      </div>
-              </div>
-              <div className=" md:mr-4 h-full bg-white rounded-[8px] lg:rounded-t-none pb-4">
-                    <InfoBar/>
-                    <div className="lg:px-6 ">
-                        <TradingViewWidget/>
-                    </div>
-              </div>
+<div className="pt-24 px-6 lg:px-12 capitalize mb-3 text-[#3E5765] flex">
+        <h1>Cryptocurrencies {`>>`} </h1>
+        <h1 className="ml-2 text-black">{`${id}`}</h1>
+    </div>
+    <div className="flex flex-col lg:flex-row px-6 lg:px-12">
 
-              <div className="lg:mr-12">
-                    <InfoHeader/>
-              </div>
+    <div className="lg:w-[70%] flex flex-col">
+          <div className="flex space-x-8 p-6 whitespace-nowrap mr-4 lg:bg-white lg:rounded-[8px] lg:rounded-b-none rounded-none">
+                  <div className="flex space-x-2 items-center">
+                    <img src={main?.image?.thumb} alt="mainLogo" />
+                        <h1 className="text-lg capitalize">{main?.name}</h1>
+                        <h3 className="text-sm capitalize">{main?.symbol}</h3>
+                  </div>
+                  <div className="bg-gray-500 p-3 text-white w-fit rounded-[8px] ">
+                        Rank #{main?.market_cap_rank}
+                  </div>
+          </div>
+          <div className=" md:mr-4 h-full bg-white rounded-[8px] lg:rounded-t-none pb-4">
+                <InfoBar/>
+                <div className="lg:mx-12 ">
+                    <TradingViewWidget/>
+                </div>
+          </div>
 
-        </div> :
-        <div className="lg:w-[70%] flex flex-col">
-          <h2>loading.....</h2>
-        </div>
-        }
+          <div className="lg:mr-[20px]">
+                <InfoHeader/>
+          </div>
 
-        
-        <div className=" lg:w-[30%]">
-              <AdComponent/>
-              <TrendingComponent/>
-        </div>
+    </div>
 
-      </div>
+    
+    <div className=" lg:w-[30%]">
+          <AdComponent/>
+          <TrendingComponent/>
+    </div>
 
-        <div className="hidden lg:flex">
-            <Footer no={5}/>
-        </div>
+    </div>
+
+    <div className="hidden lg:flex">
+        <Footer no={5}/>
+    </div>
+
+</div>:<div className="pt-24 px-6 lg:px-12  flex flex-col justify-center items-center">
+<ClipLoader
+    loading={loading}
+    size={150}
+    aria-label="Loading Spinner"
+    data-testid="loader"
+  />
+  </div>}
     </>
 
   );
 }
+
+
--- components/MainComponent/SubComponent/InfoHeader.tsx
+++ components/MainComponent/SubComponent/InfoHeader.tsx
@@ -1,46 +1,68 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import OverView from './OverView'
 import Fundamentals from './Fundamentals'
 import Sentiment from './Sentiment'
 import About from './About'
 import Tokenomics from './Tokenomics'
 import Team from './Team'
+import { Link } from 'react-scroll';
+
 
 function InfoHeader() {
+
+  const [vis, setVis] = useState()
+
+
+
   return (
-    <div className='flex flex-col  text-[#3E424A] my-6 '>
+    <div className='flex flex-col text-[#3E424A] my-6 '>
+      <div className='flex w-full justify-between overflow-y-scroll lg:overflow-y-hidden space-x-3 whitespace-nowrap border  border-[#a9aaab] border-t-0 border-x-0'>
+        <Link to='overview' smooth={true} duration={500} className='cursor-pointer border border-b-2 border-x-0 border-t-0 text-blue-600 border-blue-600 pb-3'>
+          Overview
+        </Link>
+        <Link to='overview' smooth={true} duration={500} className='cursor-pointer'>
+          Fundamentals
+        </Link>
+        <Link to='sentiment' smooth={true} duration={500} className='cursor-pointer'>
+          News Insights
+        </Link>
+        <Link to='sentiment' smooth={true} duration={500} className='cursor-pointer'>
+          Sentiments
+        </Link>
+        <Link to='team' smooth={true} duration={500} className='cursor-pointer'>
+          Team
+        </Link>
+        <Link to='tokenomics' smooth={true} duration={500} className='cursor-pointer'>
+          Technicals
+        </Link>
+        <Link to='tokenomics' smooth={true} duration={500} className='cursor-pointer'>
+          Tokenomics
+        </Link>
+      </div>
 
-      <div className='flex w-full justify-between overflow-y-scroll lg:overflow-y-hidden space-x-3 whitespace-nowrap border pb-3 border-[#a9aaab] border-t-0 border-x-0'>
-          <div className='cursor-pointer'>Overview</div>
-              <div className=' cursor-pointer'>Fundamentals</div>
-              <div className=' cursor-pointer'>News Insights</div>
-              <div className=' cursor-pointer'>Sentiments</div>
-              <div className=' cursor-pointer'>Team</div>
-              <div className=' cursor-pointer'>Technicals</div>
-              <div className=' cursor-pointer'>Tokenomics</div>
-          </div>
 
 
-
-          <div className='bg-white rounded-[8px] p-6 mt-6'>
+          <div id='overview' className='bg-white rounded-[8px] p-6 mt-6'>
             <OverView/>
             <Fundamentals/>
           </div>
 
 
-          <div className='bg-white rounded-[8px] p-6 mt-6'>
+          <div id='sentiment' className='bg-white rounded-[8px] p-6 mt-6'>
             <Sentiment/>
           </div>
 
-          <div className='bg-white rounded-[8px] p-6 mt-6'>
+          <div id='about' className='bg-white rounded-[8px] p-6 mt-6'>
             <About/>
           </div>
 
-          <div className='hidden lg:flex flex-col bg-white rounded-[8px] p-6 mt-6'>
+          <div id='tokenomics'  className='hidden lg:flex flex-col bg-white rounded-[8px] p-6 mt-6'>
             <Tokenomics/>
           </div>
 
-          <div className='bg-white rounded-[8px] p-6 mt-6'>
+          <div id='team' className='bg-white rounded-[8px] p-6 mt-6'>
             <Team/>
           </div>
     </div>
